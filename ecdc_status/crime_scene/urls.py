410ce53f5cccd54d7ebf6d21dba0fbb46e097ad5
Jonas Nilsson <mjc.nilsson@gmail.com>
2019-01-26 14:38:50 +0100

    Added list of crime scenes.

--- ecdc_status/crime_scene/urls.py
+++ ecdc_status/crime_scene/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.list),
     path('random/', views.random_crime_scene),
     path('<int:id>/', views.crime_scene),
     path('data/<int:id>', views.crime_scene_data, name="crime_scene_data"),
