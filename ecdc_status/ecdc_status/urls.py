dbf150303b04efe19012e4c860159c482bbc4e63
Jonas Nilsson <mjc.nilsson@gmail.com>
2019-01-26 13:38:21 +0100

    Made icons use FileField.

--- ecdc_status/ecdc_status/urls.py
+++ ecdc_status/ecdc_status/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('upcoming_events/', include('upcoming_events.urls')),
     path('crime_scene/', include('crime_scene.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
