5c7dfbc5fd8330638d959b92d03e9cbe9969cad8
Jonas Nilsson <jonas.nilsson@esss.se>
2020-01-14 11:34:24 +0100

    Added basic template.

--- ecdc_status/maxims/views.py
+++ ecdc_status/maxims/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from random import randint
 from .models import Maxim
 from django.http import HttpResponse
+from django.template import loader
 
 def random_raw_maxim(request):
     AllMaxims = Maxim.objects.all()
@@ -11,7 +12,15 @@ def random_raw_maxim(request):
 def raw_maxim(request, Id):
     CurrentMaxim = Maxim.objects.get(Id= int(Id))
     return HttpResponse("#{}: {}".format(CurrentMaxim.Id, CurrentMaxim.Text))
+    
+def html_maxim(request, Id):
+    CurrentMaxim = Maxim.objects.get(Id= int(Id))
+    template = loader.get_template('maxim.html')
+    context = {"id": int(Id), "text":CurrentMaxim.Text}
+    return HttpResponse(template.render(context, request))
 
 def random_maxim(request):
-    return random_raw_maxim(request)
+    AllMaxims = Maxim.objects.all()
+    RandomIndex = randint(0, len(AllMaxims) - 1)
+    return html_maxim(request, AllMaxims[RandomIndex].Id)
     
